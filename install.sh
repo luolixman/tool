a8424779c814973c545f63fd84c30fa2562728a3
luolixman <59561026+luolixman@users.noreply.github.com>
2022-10-09 11:21:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,31 @@
 
 #主程序开始
 echo "Satrt Running"
-ls
+checkSystem() {
+  if [[ -n $(find /etc -name "redhat-release") ]] || grep </proc/version -q -i "centos"; then
+    # 检测系统版本号
+    centosVersion=$(rpm -q centos-release | awk -F "[-]" '{print $3}' | awk -F "[.]" '{print $1}')
+    if [[ -z "${centosVersion}" ]] && grep </etc/centos-release "release 8"; then
+      centosVersion=8
+    fi
+    release="centos"
+
+  elif grep </etc/issue -q -i "debian" && [[ -f "/etc/issue" ]] || grep </etc/issue -q -i "debian" && [[ -f "/proc/version" ]]; then
+    if grep </etc/issue -i "8"; then
+      debianVersion=8
+    fi
+    release="debian"
+
+  elif grep </etc/issue -q -i "ubuntu" && [[ -f "/etc/issue" ]] || grep </etc/issue -q -i "ubuntu" && [[ -f "/proc/version" ]]; then
+    release="ubuntu"
+  fi
+
+  if [[ -z ${release} ]]; then
+    echo "其他系统"
+    exit 0
+  else
+    echo "当前系统为${release}"
+  fi
+}
+checkSystem
 echo "End"
