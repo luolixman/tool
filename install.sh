bc5168995360648e231317cb9045c97bc1e0f0d8
luolixman <59561026+luolixman@users.noreply.github.com>
2022-10-09 11:35:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,33 +2,6 @@
 
 #主程序开始
 echo "Satrt Running"
-checkSystem() {
-  if [[ -n $(find /etc -name "redhat-release") ]] || grep </proc/version -q -i "centos"; then
-    # 检测系统版本号
-    centosVersion=$(rpm -q centos-release | awk -F "[-]" '{print $3}' | awk -F "[.]" '{print $1}')
-    if [[ -z "${centosVersion}" ]] && grep </etc/centos-release "release 8"; then
-      centosVersion=8
-    fi
-    release="centos"
-
-  elif grep </etc/issue -q -i "debian" && [[ -f "/etc/issue" ]] || grep </etc/issue -q -i "debian" && [[ -f "/proc/version" ]]; then
-    if grep </etc/issue -i "8"; then
-      debianVersion=8
-    fi
-    release="debian"
-
-  elif grep </etc/issue -q -i "ubuntu" && [[ -f "/etc/issue" ]] || grep </etc/issue -q -i "ubuntu" && [[ -f "/proc/version" ]]; then
-    release="ubuntu"
-  fi
-
-  if [[ -z ${release} ]]; then
-    echo "其他系统"
-    exit 0
-  else
-    echo "当前系统为${release}"
-  fi
-}
-checkSystem
 
 startInstallPanel() {
   #安装更新运行环境（Debian系统）
@@ -45,5 +18,37 @@ startInstallPanel() {
   #最后执行下一键优化补丁
   wget -O optimize.sh http://f.cccyun.cc/bt/optimize.sh && bash optimize.sh
 }
+
+checkSystem() {
+  if [[ -n $(find /etc -name "redhat-release") ]] || grep </proc/version -q -i "centos"; then
+    # 检测系统版本号
+    centosVersion=$(rpm -q centos-release | awk -F "[-]" '{print $3}' | awk -F "[.]" '{print $1}')
+    if [[ -z "${centosVersion}" ]] && grep </etc/centos-release "release 8"; then
+      centosVersion=8
+    fi
+    release="centos"
+
+  elif grep </etc/issue -q -i "debian" && [[ -f "/etc/issue" ]] || grep </etc/issue -q -i "debian" && [[ -f "/proc/version" ]]; then
+    if grep </etc/issue -i "8"; then
+      debianVersion=8
+    fi
+    startInstallPanel
+    release="debian"
+
+  elif grep </etc/issue -q -i "ubuntu" && [[ -f "/etc/issue" ]] || grep </etc/issue -q -i "ubuntu" && [[ -f "/proc/version" ]]; then
+    startInstallPanel
+    release="ubuntu"
+  fi
+
+  if [[ -z ${release} ]]; then
+    echo "其他系统"
+    exit 0
+  else
+    echo "当前系统为${release}"
+  fi
+}
+checkSystem
+
+
 
 echo "End"
